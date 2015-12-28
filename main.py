f15c8a8ee08e25e4d97b5ebe3e4b38b9829f4c5a
TomHortons <imanishi@west.sd.keio.ac.jp>
2015-12-28 21:06:46 +0900

    added xgb.Classifier

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-__author__ = 'tomoyaimanishi'
+#reference https://www.kaggle.com/mpearmain/homesite-quote-conversion/xgboost-benchmark
+#about xgboost.Classifier https://github.com/dmlc/xgboost/blob/7a74c9523ad9eecae0b99a48a7d2502b8f49cb1b/python-package/xgboost/sklearn.py
+
